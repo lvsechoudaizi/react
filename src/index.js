efeb9ea5a51d2339e7517e3c6c1941ef4f2a14d2
李建华 <jianhua.li@hualongdata.com>
2018-10-29 17:59:31 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import 'babel-polyfill';
+import App from './App'
+
+const renderDom = Component => {
+  render(
+    <Component />,
+    document.getElementById('app')
+  );
+}
+renderDom(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const App = require('./App').default;
+    renderDom(App);
+  })
+}
---
+++ webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  mode: 'production',
+  devtool: 'cheap-module-eval-source-map',
+  //entry: path.resolve(__dirname, '../src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
+  entry: [
+    'webpack-dev-server/client?http://localhost:9090',
+    'webpack/hot/only-dev-server',
+    path.resolve(__dirname, '../src/index.js')
+  ],
+
+  output: {
+    path: path.resolve(__dirname, '../dist'), // 输出的路径
+    filename: 'app.[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../index.html'),
+      inject: true
+    })
+  ]
+}
