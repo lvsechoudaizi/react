72b84f5341fedd78fa3f2198c69a5f0b3fa66dac
李建华 <jianhua.li@hualongdata.com>
2018-10-31 11:13:30 +0800

    项目搭建

--- config.js
+++ config.js
@@ -2,6 +2,71 @@ const path = require('path')
 
 module.exports = {
   build: {
-
+    env: {
+      NODE_ENV: 'production',
+      CLIENT: true,
+      SERVER: false,
+    },
+    entry: {
+      app: path.resolve(__dirname, './src/index'),
+      vendor: [
+        'babel-polyfill',
+        'react',
+        'react-dom',
+        'react-router-dom',
+        'mobx',
+        'mobx-react',
+      ],
+      html: path.resolve(__dirname, './index.html'),
+      srcRoot: path.resolve(__dirname, './src'),
+      staticRoot: path.resolve(__dirname, './static'),
+      styleRoot: path.resolve(__dirname, './src/styles'),
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      staticRoot: path.resolve(__dirname, './dist/static'),
+      publicPath: '/',
+    },
+    plugins: {
+      CopyWebpackPlugin: [
+        {
+          from: path.resolve(__dirname, './static'),
+          to: path.resolve(__dirname, './dist/static'),
+          ignore: ['html/loading/*.*', 'html/login/*.*'],
+        },
+      ],
+      ReactLoadablePlugin: {
+        filename: path.resolve(__dirname, './dist/react-loadable.json'),
+      },
+    },
+  },
+  dev: {
+    styleRoot: path.resolve(__dirname, './src/styles'),
+    utilRoot: path.resolve(__dirname, './src/utils'),
+    componentRoot: path.resolve(__dirname, './src/components/components.js'),
+    env: {
+      NODE_ENV: 'development',
+      PRODUCTION_ENV: 'production',
+      CLIENT: true,
+      SERVER: false,
+    },
+    devServer: {
+      port: 8080,
+    },
+    entry: {
+      app: path.resolve(__dirname, './src/index'),
+      ssrApp: path.resolve(__dirname, './src/server/index'),
+      html: path.resolve(__dirname, './index.html'),
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '/',
+    },
+  },
+  root: path.resolve(__dirname),
+  modifyVars: {
+    'icon-url': "'/static/antd/iconfont/iconfont'",
+    '@border-radius-base': '2px',
+    '@form-item-margin-bottom': '12px',
   },
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,23 +1,57 @@
-const path = require('path')
+/* eslint-disable */
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const config = require('../config')
+
+const proxy = '192.168.32.104'
 
 module.exports = {
-  mode: 'production',
+  mode: 'development', // "production" | "development" | "none"
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'babel-polyfill',
-    'webpack-dev-server/client?http://localhost:9090',
-    'webpack/hot/only-dev-server',
-    path.resolve(__dirname, '../src/index.js'),
+    // 'webpack-dev-server/client?http://localhost:9090',
+    // 'webpack/hot/only-dev-server',
+    config.dev.entry.app,
   ],
-
   output: {
-    path: path.resolve(__dirname, '../dist'), // 输出的路径
+    path: config.dev.output.path,
+    publicPath: config.dev.output.publicPath,
     filename: 'app.[hash].js',
+    chunkFilename: '[name].[chunkhash:4].chunk.js',
+  },
+  // 在使用import时 可以直接通过别名引入
+  resolve: {
+    alias: {
+      moment$: 'moment/moment.js',
+      Styles: config.dev.styleRoot,
+      Utils: config.dev.utilRoot,
+      Components$: config.dev.componentRoot,
+    },
   },
   performance: {
     hints: false,
+  },
+  devServer: {
+    compress: true,
+    hot: true,
+    inline: true,
+    host: '0.0.0.0',
+    publicPath: '/',
+    contentBase: config.root,
+    port: config.dev.devServer.port,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    proxy: {
+      '/api': {
+        target: `http://${proxy}:41000`,
+        changeOrigin: true,
+      },
+      '/res': {
+        target: `http://${proxy}:41000`,
+        changeOrigin: true,
+      },
+    },
   },
   module: {
     rules: [
@@ -27,18 +61,63 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        enforce: 'pre',
-        test: /\.(js|jsx)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/,
+        test: /\.less|css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+              modifyVars: config.modifyVars,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              progressive: true,
+              optimizationLevel: 7,
+              interlaced: false,
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'file-loader',
       },
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(config.dev.env.NODE_ENV),
+      },
+    }),
+    new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../index.html'),
-      inject: true,
+      hash: false,
+      template: config.dev.entry.html,
+      chunksSortMode: 'none',
     }),
   ],
 }
